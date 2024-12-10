719c6e431f91a80af4b55db5938cfa1dfa1aacfe
PryVlad <pr.vl@outlook.com>
2024-12-10 13:15:55 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Vladyslav Pryl on 10.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
